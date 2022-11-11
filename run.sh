8e5268673455ea1c103aadaed5ea139abccdd230
Khánh Pluto <tranvankhanh99a@gmail.com>
2022-11-11 09:35:40 +0700

    Paddle recognition vietnamese

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 150 -l vi -b 1 -r -f 100 -t 3 -wk
-python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 550 -l vi -b 3 -r -f 100 -t 3 -wk
-python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 150 -l vi -b 0 -r -f 100 -t 3 -wk
-python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 150 -l vi -b 2 -r -f 100 -t 3 -wk
+python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 1500 -l vi -b 1 -r -f 100 -t 20 -wk
+python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 5500 -l vi -b 3 -r -f 100 -t 20 -wk
+python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 1500 -l vi -b 0 -r -f 100 -t 20 -wk
+python run.py -w 10 -bl 2 -rbl -k 1 -rk -c 1500 -l vi -b 2 -r -f 100 -t 20 -wk
